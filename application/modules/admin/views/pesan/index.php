931bc5b06921606372fe7efc8e3b0872ba69b0c0
sultand28 <28.sultandirgapratama@gmail.com>
2019-07-22 14:49:15 +0800

    sembarang

---
+++ application/modules/admin/views/pesan/index.php
@@ -0,0 +1,250 @@
+<div class="col-xs-12">
+    <div class="wt-haslayout wt-dbsectionspace">
+        <div class="wt-dashboardbox wt-dashboardtabsholder">
+            <div class="wt-dashboardboxtitle">
+                <h2>Notifikasi</h2>
+            </div>
+
+
+            <div class="wt-personalskillshold row">
+
+                <div class="wt-yourdetails wt-tabsinfo col-12 col-xl-6">
+                    <div class="wt-tabscontenttitle">
+                        <h2>Kirim Pesan ke Tentor</h2>
+                    </div>
+                    <form class="wt-formtheme wt-userform">
+                        <fieldset>
+                            <div class="form-group">
+                                <input type="text" name="tagline" class="form-control" placeholder="Username Tentor">
+                            </div>
+                            <div class="form-group">
+                                <textarea name="message" class="form-control" placeholder="Pesan . . ."></textarea>
+                            </div>
+                            <div class="form-group text-center">
+                                <a href="" class="wt-btn float-sm-right">Kirim Pesan</a>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+
+                <div class="wt-yourdetails wt-tabsinfo col-12 col-xl-6">
+                    <div class="wt-tabscontenttitle">
+                        <h2>Kirim Pesan ke Siswa</h2>
+                    </div>
+                    <form class="wt-formtheme wt-userform">
+                        <fieldset>
+                            <div class="form-group">
+                                <input type="text" name="tagline" class="form-control" placeholder="Username Siswa">
+                            </div>
+                            <div class="form-group">
+                                <textarea name="message" class="form-control" placeholder="Pesan . . ."></textarea>
+                            </div>
+                            <div class="form-group text-center">
+                                <a href="" class="wt-btn float-sm-right">Kirim Pesan</a>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+
+                <div class="wt-yourdetails wt-tabsinfo col-12">
+                    <div class="wt-tabscontenttitle">
+                        <h2>Pesan Masuk</h2>
+                    </div>
+                    <ul class="wt-navarticletab nav navbar-nav mb-3">
+                        <li class="nav-item">
+                            <a class="active show" id="all-tab" data-toggle="tab" href="#rtk">Request Tambah Kompetensi</a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="business-tab" data-toggle="tab" href="#rp" class="">Request Cairkan Poin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="business-tab" data-toggle="tab" href="#rtp" class="">Request Tambah Poin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="business-tab" data-toggle="tab" href="#ua" class="">Upload Absen</a>
+                        </li>
+
+                    </ul>
+                    <form class="wt-formtheme wt-userform tab-content wt-haslayout">
+                        <div class="wt-contentarticle tab-pane fade active show" id="rtk">
+                            <div class="table-responsive">
+                                <table class="table table-hover DataTables">
+                                    <thead style="background-color:#ff5851" class="text-white">
+                                        <tr>
+                                            <th width="30px">No</th>
+                                            <th>Username Tentor</th>
+                                            <th>Kategori</th>
+                                            <th>Kursus</th>
+                                            <th>Jenjang</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>sultand28</td>
+                                            <td>Olahraga</td>
+                                            <td>Renang</td>
+                                            <td>SMP</td>
+                                            <td>oke</td>
+                                        </tr>
+                                        <tr>
+                                            <th>2</th>
+                                            <td>aswar123</td>
+                                            <td>Pengetahuan</td>
+                                            <td>Matematika</td>
+                                            <td>SMA</td>
+                                            <td>oke</td>
+                                        </tr>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>afief89</td>
+                                            <td>Komputer</td>
+                                            <td>Merakit Komputer</td>
+                                            <td>SMA</td>
+                                            <td>oke</td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="wt-contentarticle tab-pane fade" id="rp">
+                            <div class="table-responsive">
+                                <table class="table table-hover DataTables">
+                                    <thead style="background-color:#ff5851" class="text-white">
+                                        <tr>
+                                            <th width="30px">No</th>
+                                            <th>Tentor</th>
+                                            <th>No. Rekening</th>
+                                            <th>Jumlah Deposit</th>
+                                            <th>Deposit Ingin Dicairkan</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>Sultan Dirga</td>
+                                            <td>xxxxxxxx</td>
+                                            <td>RP. 1.200.000</td>
+                                            <td>Rp. 1.000.000</td>
+                                            <td>oke</td>
+                                        </tr>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>Zuhdi Afief</td>
+                                            <td>xxxxxxxx</td>
+                                            <td>RP. 12.200.000</td>
+                                            <td>Rp. 5.000.000</td>
+                                            <td>oke</td>
+                                        </tr>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>Aswar Kasim</td>
+                                            <td>xxxxxxxx</td>
+                                            <td>RP. 7.200.000</td>
+                                            <td>Rp. 200.000</td>
+                                            <td>oke</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+
+                        <div class="wt-contentarticle tab-pane fade" id="rtp">
+                            <div class="table-responsive">
+                                <table class="table table-hover DataTables">
+                                    <thead style="background-color:#ff5851" class="text-white">
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Tanggal</th>
+                                            <th>Tentor</th>
+                                            <th>Jumlah Poin</th>
+                                            <th>Request Poin</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>17-07-2019</td>
+                                            <td>Sultan Dirga</td>
+                                            <td>RP. 0</td>
+                                            <td>Rp. 200.000</td>
+                                            <td> <a href="">Lihat Detail</a> </td>
+                                        </tr>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>17-07-2019</td>
+                                            <td>Zuhdi Afief</td>
+                                            <td>RP. 0</td>
+                                            <td>Rp. 200.000</td>
+                                            <td> <a href="">Lihat Detail</a> </td>
+                                        </tr>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>17-07-2019</td>
+                                            <td>Aswar Kasim</td>
+                                            <td>RP. 0</td>
+                                            <td>Rp. 200.000</td>
+                                            <td> <a href="">Lihat Detail</a> </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+
+                        <div class="wt-contentarticle tab-pane fade" id="ua">
+                            <div class="table-responsive">
+                                <table class="table table-hover DataTables">
+                                    <thead style="background-color:#ff5851" class="text-white">
+                                        <tr>
+                                            <th width="30px">No</th>
+                                            <th>Tentor</th>
+                                            <th>Siswa</th>
+                                            <th>Kursus</th>
+                                            <th>Paket</th>
+                                            <th>Bukti Foto</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>1</th>
+                                            <td>Sultan Dirga</td>
+                                            <td>Aswar Kasim</td>
+                                            <td>Renang</td>
+                                            <td>Paket 6</td>
+                                            <td></td>
+                                            <td><span class="badge badge-danger">Belum Selesai</span></td>
+                                            <td>oke</td>
+                                        </tr>
+                                        <tr>
+                                            <th>2</th>
+                                            <td>Afief Zuhdi</td>
+                                            <td>Aswar Kasim</td>
+                                            <td>Matematika</td>
+                                            <td>Paket 1</td>
+                                            <td></td>
+                                            <td><span class="badge badge-success">Selesai</span></td>
+                                            <td>oke</td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
